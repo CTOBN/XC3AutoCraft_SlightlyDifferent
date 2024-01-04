a2a8ed240e7108cc5ef32f2c9ac0ab546eacca18
eight <hgrmtr8@gmail.com>
2024-01-04 18:20:19 +0900

    fix magic number

--- XC3StateDetector/source/Setting.cpp
+++ XC3StateDetector/source/Setting.cpp
@@ -163,7 +163,7 @@ void Setting::setProbability()
 			probabilityTable.setText(i + 1, j, U"{:.5f}"_fmt(probability));
 			sumProbabilityList[j - 1] += probability;
 			sumProbabilityList[j - 1] = Min(sumProbabilityList[j - 1], 100.0);
-			probabilityTable.setText(6, j, U"{:.5f}"_fmt(sumProbabilityList[j - 1]));
+			probabilityTable.setText(TARGET_ACCSESORIES_COUNT_MAX + 1, j, U"{:.5f}"_fmt(sumProbabilityList[j - 1]));
 		}
 	}
 }
