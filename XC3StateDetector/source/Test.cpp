334eefbd666b2258eef64d7567852ce01277fcee
eight <hgrmtr8@gmail.com>
2024-01-14 12:16:53 +0900

    Test.cpp追加

---
+++ XC3StateDetector/source/Test.cpp
@@ -0,0 +1,9 @@
+﻿#include <ThirdParty/Catch2/catch.hpp>
+#include "Accessory.hpp"
+
+// アクセサリーの比較関数のテスト
+TEST_CASE("Accessory comparison function test", "[Accessory]")
+{
+	// アクセサリーの生成
+	Accessory accessory1{ 1 };
+}
